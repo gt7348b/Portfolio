e1b89cfd2c8c1d566340693e0130aa7168aa5bbf
John Crocker <johnniecrocker@yahoo.com>
2014-12-29 14:03:05 -0500

    gulped

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+console.log('Roscoe plays peekaboo');
+
+(function(){
+
+  angular.module('Portfolio', ['ngResource', 'ngRoute'])
+
+  .config(function($routeProvider){
+
+    $routeProvider.when('/', {
+      templateUrl: 'templates/home.html',
+      controller: 'MainController'
+    });
+
+    $routeProvider.when('/portfolio', {
+      templateUrl: 'templates/portfolio.html',
+      controller: 'PortfolioController'
+    });
+
+    $routeProvider.when('/library', {
+      templateUrl: 'templates/library.html',
+      controller: 'MainController'
+    });
+
+    $routeProvider.when('/about', {
+      templateUrl: 'templates/about.html',
+      controller: 'MainController'
+    });
+
+
+  })
+
+}());
